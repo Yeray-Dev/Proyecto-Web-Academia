d289a269eeac9f57f20be2541f5a27c7d51adf2c
Yeray-Dev <wow_ybf@hotmail.es>
2025-06-11 13:38:30 +0200

    Workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://yeray-dev.github.io",
+  base: "Proyecto-Web-Academia",
+})
