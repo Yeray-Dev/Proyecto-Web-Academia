7e53a2388665fe8ef9b5241704948dcf853ec452
Yeray-Dev <wow_ybf@hotmail.es>
2025-06-12 11:10:30 +0200

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  site: "https://yeray-dev.github.io",
-  base: "Proyecto-Web-Academia",
+  site: 'https://yeray-dev.github.io',
+  base: '/Proyecto-Web-Academia/',
+
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
+
